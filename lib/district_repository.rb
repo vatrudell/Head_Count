afc8a5bfcd03e9a82be67135b7a4b2bd4bf011a7
Valerie Trudell <v.trudell@protonmail.com>
2017-01-04 16:28:33 -0700

    Adds load_name and find by name for district_repository for iteration_0

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,16 +1,36 @@
 require 'csv'
 require 'pry'
 
+
 class DistrictRepository
+  # attr_accessor :location,
+  #               :timeframe,
+  #               :dataformat,
+  #               :data
+  #
+  def initialize
+    #@whole_processed_file = whole_processed_file
+  end
+  #
+
   def load_data(input)
     file = input[:enrollment][:kindergarten]  #thats probably hardcoded in there
     data = CSV.open(file, :headers => true, :header_converters => :symbol)
+    @whole_processed_file = data.map do |line|
+      line
+    end
+    #puts @whole_processed_file[0]
+
+
   end
 
+  #binding.pry
   def find_by_name(district_name)
-    "I don't fucking know"
-    #have questions on this
-    #
+    @whole_processed_file.find_all do |line|
+      if line[0] == district_name
+        puts line
+      end
+    end
   end
 end
 
@@ -20,3 +40,11 @@ end
 #     :kindergarten => "./data/Kindergartners in full-day program.csv"
 #   }
 # })
+
+derp = DistrictRepository.new
+derp.load_data({
+  :enrollment => {
+    :kindergarten => "./data/Kindergartners in full-day program.csv"
+  }
+})
+derp.find_by_name("ACADEMY 20")
