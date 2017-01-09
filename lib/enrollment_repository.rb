218d00c10e59e98e74a6babed56d8438cd9e80a7
Valerie Trudell <v.trudell@protonmail.com>
2017-01-08 21:12:35 -0700

    Begginning of iteration_2 adds data_parser class, changes the way populate data recieves files

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -8,22 +8,27 @@ class EnrollmentRepository
     @enrollments = {}
   end
 
-  def load_data(input)
-    file = input[:enrollment][:kindergarten]  #thats probably hardcoded in there
-    @data = CSV.open(file, headers: true, header_converters: :symbol)
-    populate_data
+  def load_data({:enrollment => :kindergarten => nil, :high_school_graduation => nil})
+    kindergarten = input[:enrollment][:kindergarten]
+    highschool = input[:enrollment][:high_school_graduation]
+    #binding.pry
+    #file = input[:enrollment][:kindergarten]  #thats probably hardcoded in there
+    #@data = CSV.open(file, headers: true, header_converters: :symbol)
+
   end
 
-  def populate_data
+  def populate_data #kindergarten
     @data.each do |line|
       if @enrollments[line[:location].upcase]
         @enrollments[line[:location].upcase].kindergarten_participation[line[:timeframe].to_i] = line[:data][0..4].to_f
+        binding.pry
       else
         @enrollments[line[:location].upcase] = Enrollment.new({name: line[:location].upcase, kindergarten_participation: {line[:timeframe].to_i => line[:data][0..4].to_f}})
       end
     end
-    #binding.pry
   end
+
+
 
   def find_by_name(district_name)
     @enrollments[district_name]
@@ -31,19 +36,11 @@ class EnrollmentRepository
 end
 
 
-# er = EnrollmentRepository.new
-# er.load_data({
-#          :enrollment => {
-#            :kindergarten => "./data/Kindergartners in full-day program.csv"
-#          }
-#        })
-# er.find_by_name("COLORADO")
-
-#
-#
-# er.load_data({
-#   :enrollment => {
-#     :kindergarten => "./data/Kindergartners in full-day program.csv"
-#   }
-# })
-# enrollment = er.find_by_name("ACADEMY 20")
+er = EnrollmentRepository.new
+er.load_data({
+         :enrollment => {
+           :kindergarten => "./data/Kindergartners in full-day program.csv",
+           :high_school_graduation => "./data/High school graduation rates.csv"
+         }
+       })
+enrollment = er.find_by_name("ACADEMY 20")
