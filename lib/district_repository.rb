57eebb82c6a91199c37f639ffeab4981670616c5
Valerie Trudell <v.trudell@protonmail.com>
2017-01-10 15:49:20 -0700

    Adds testing and cleans files

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -6,61 +6,38 @@ require './lib/enrollment_repository'
 
 
 class DistrictRepository
-  attr_reader :districts,
-              :enrollment_instance,
-              :file
+  attr_accessor :districts,
+                :enrollment_instance,
+                :file,
+                :find_enrollment
 
   def initialize
     @districts = {}
     @enrollment_instance = EnrollmentRepository.new
   end
 
-
-def load_data(input)
-  input[:enrollment].each do |name, value|
+  def load_data(input)
+    input[:enrollment].each do |name, value|
       file = input[:enrollment][name]
-      @data = CSV.open(file, headers: true, header_converters: :symbol)
-        @data.each do |line|
-          if @districts[line[:location].upcase]
-            if name == :kindergarten
-              @districts[line[:location].upcase].enrollment_data[line[:timeframe].to_i] = line[:data][0..4].to_f
-            elsif
-              name == :high_school_graduation
-              @districts[line[:location].upcase].graduation_data[line[:timeframe].to_i] = line[:data][0..4].to_f
-            end
-          else
-            if name == :kindergarten
-              @districts[line[:location].upcase] = District.new({name: line[:location].upcase, enrollment_data: {line[:timeframe].to_i => line[:data][0..4].to_f}})
-            elsif name == :high_school_graduation
-              @districts[line[:location].upcase] = District.new({name: line[:location].upcase, graduation_data: {line[:timeframe].to_i => line[:data][0..4].to_f}})
-            end
+    @data = CSV.open(file, headers: true, header_converters: :symbol)
+      @data.each do |line|
+        if @districts[line[:location].upcase]
+          if name == :kindergarten
+            @districts[line[:location].upcase].enrollment_data[line[:timeframe].to_i] = line[:data][0..4].to_f
+          elsif
+            name == :high_school_graduation
+            @districts[line[:location].upcase].graduation_data[line[:timeframe].to_i] = line[:data][0..4].to_f
+          end
+        else
+          if name == :kindergarten
+            @districts[line[:location].upcase] = District.new({name: line[:location].upcase, enrollment_data: {line[:timeframe].to_i => line[:data][0..4].to_f}}, self)
+          elsif name == :high_school_graduation
+            @districts[line[:location].upcase] = District.new({name: line[:location].upcase, graduation_data: {line[:timeframe].to_i => line[:data][0..4].to_f}}, self)
           end
         end
       end
-    # binding.pry
+    end
   end
-
-
-
-  # def load_data(input)
-  #   file = input[:enrollment][:kindergarten]  #thats probably hardcoded in there
-  #   @file = CSV.open(file, headers: true, header_converters: :symbol)
-  #   @enrollment_instance.load_data(input)
-  #   populate_data
-  # end
-
-
-  # def populate_data
-  #   @file.each do |line|
-  #     if @districts[line[:location].upcase]
-  #       @districts[line[:location].upcase].enrollment_data[line[:timeframe].to_i] = line[:data][0..4].to_f
-  #     else
-  #       @districts[line[:location].upcase] = District.new({name: line[:location].upcase, enrollment_data: {line[:timeframe].to_i => line[:data][0..4].to_f}}, self)
-  #       #binding.pry
-  #     end
-  #   end
-  #   # binding.pry
-  # end
 
   def find_by_name(district_name)
     @districts[district_name]
@@ -81,11 +58,3 @@ end
     # dr = DistrictRepository.new
     # dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv",
     #                               :high_school_graduation => "./data/High school graduation rates.csv"}})
-
-# dr = DistrictRepository.new
-# dr.load_data({
-#                :enrollment => {
-#                  :kindergarten => "./data/Kindergartners in full-day program.csv"
-#                }
-#              })
-# puts dr.find_by_name("ACADEMY 20")
--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 class EnrollmentRepository
   attr_accessor :enrollments,
-                :data_tag,
                 :data
   def initialize
     @enrollments = {}
@@ -12,41 +11,28 @@ class EnrollmentRepository
 
  def load_data(input)
   input[:enrollment].each do |name, value|
-      file = input[:enrollment][name]
-      @data = CSV.open(file, headers: true, header_converters: :symbol)
-        @data.each do |line|
-          if @enrollments[line[:location].upcase]
-            if name == :kindergarten
-              @enrollments[line[:location].upcase].kindergarten_participation[line[:timeframe].to_i] = line[:data][0..4].to_f
-            elsif
-              name == :high_school_graduation
-              @enrollments[line[:location].upcase].high_school_graduation[line[:timeframe].to_i] = line[:data][0..4].to_f
-            end
-          else
-            if name == :kindergarten
-              @enrollments[line[:location].upcase] = Enrollment.new({name: line[:location].upcase, kindergarten_participation: {line[:timeframe].to_i => line[:data][0..4].to_f}})
-            elsif name == :high_school_graduation
-              @enrollments[line[:location].upcase] = Enrollment.new({name: line[:location].upcase, high_school_graduation: {line[:timeframe].to_i => line[:data][0..4].to_f}})
-            end
+    file = input[:enrollment][name]
+    @data = CSV.open(file, headers: true, header_converters: :symbol)
+      @data.each do |line|
+        if @enrollments[line[:location].upcase]
+          if name == :kindergarten
+            @enrollments[line[:location].upcase].kindergarten_participation[line[:timeframe].to_i] = line[:data][0..4].to_f
+          elsif
+            name == :high_school_graduation
+            @enrollments[line[:location].upcase].high_school_graduation[line[:timeframe].to_i] = line[:data][0..4].to_f
+          end
+        else
+          if name == :kindergarten
+            @enrollments[line[:location].upcase] = Enrollment.new({name: line[:location].upcase, kindergarten_participation: {line[:timeframe].to_i => line[:data][0..4].to_f}})
+          elsif name == :high_school_graduation
+            @enrollments[line[:location].upcase] = Enrollment.new({name: line[:location].upcase, high_school_graduation: {line[:timeframe].to_i => line[:data][0..4].to_f}})
           end
         end
       end
-    # binding.pry
+    end
   end
 
   def find_by_name(district_name)
     @enrollments[district_name]
   end
-
 end
-
-# er = EnrollmentRepository.new
-# er.load_data({
-#          :enrollment => {
-#            :kindergarten => "./data/Kindergartners in full-day program.csv",
-#            :high_school_graduation => "./data/High school graduation rates.csv"
-#          }
-#        })
-
-# e = er.find_by_name("COLORADO")
-# e.graduation_rate_by_year
